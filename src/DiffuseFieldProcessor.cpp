6ae695c5da0fbb7d07545ac6d6adc17d2eddc959
5tr0bl <83403693+5tr0bl@users.noreply.github.com>
2024-03-01 14:11:43 +0100

    Initial commit

---
+++ src/DiffuseFieldProcessor.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    DiffuseFieldProcessor.cpp
+    Created: 30 Jan 2024 11:10:34am
+    Author:  Micha
+
+  ==============================================================================
+*/
+
+#include "DiffuseFieldProcessor.h"
+
+DiffuseFieldProcessor::DiffuseFieldProcessor()
+{
+}
+
+DiffuseFieldProcessor::~DiffuseFieldProcessor()
+{
+}
+
+// binRev and weighting function should already have been provided by JSON import
+void DiffuseFieldProcessor::init(juce::dsp::ProcessSpec& spec, float mixingTime)
+{
+    int mixingTimeInSamples = (int)((mixingTime / 1000) * spec.sampleRate); // mixing time gets passed in as milliseconds
+    mixingTimeInSamples *= 2; // see synthesizeBRIR.m l.37
+    mixingTimeInSamples = inverseWeightingFunc.getNumSamples(); // this seems to work better, matches Johannes' value
+    
+    int binRevNumSamples = binRevBuffer.getNumSamples();
+    int binRevExcerptNumSamples = binRevNumSamples - mixingTimeInSamples;
+
+    juce::AudioBuffer<float> binRevWeighted(binRevBuffer.getNumChannels(), binRevNumSamples);
+    binRevWeighted.copyFrom(0, 0, binRevBuffer, 0, 0, binRevNumSamples);
+    
+    // beginning of binRevBuffer will be modified by wfInv values, for the later part wfInv = 1
+    for (int channel = 0; channel < binRevWeighted.getNumChannels(); ++channel) {
+        for (int sample = 0; sample < binRevWeighted.getNumSamples(); ++sample) {
+            if (sample < mixingTimeInSamples) {
+                binRevWeighted.setSample(channel, sample, binRevWeighted.getSample(channel, sample) * inverseWeightingFunc.getSample(0, sample));
+            }
+        }
+    }
+    
+    // temporary buffer's ownership is moved to convolver
+    convolver.loadImpulseResponse(std::move(binRevWeighted), spec.sampleRate, juce::dsp::Convolution::Stereo::yes, juce::dsp::Convolution::Trim::no, juce::dsp::Convolution::Normalise::no);
+    convolver.reset();
+    convolver.prepare(spec);
+    auto irs = convolver.getCurrentIRSize(); // debug purpose
+}
+
+// inverted weighting function already applied in init()
+void DiffuseFieldProcessor::process()
+{
+    // inputBuffer has been filled prior within PluginProcessor.processBlock()
+    juce::dsp::AudioBlock<float> blockToProcess = juce::dsp::AudioBlock<float>(inputBuffer);
+    juce::dsp::ProcessContextReplacing<float> context(blockToProcess);
+    convolver.process(context);
+}
